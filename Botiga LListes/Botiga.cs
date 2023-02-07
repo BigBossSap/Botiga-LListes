890a3c569aecc4ff6096da57e63076192aebfed2
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2023-02-07 10:56:55 +0100

    Add project files.

---
+++ Botiga LListes/Botiga.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Botiga_LListes
+{
+    internal class Botiga
+
+
+    {
+
+        string nom_botiga { get; set; }
+
+        private List<Producte> prestatge = new List<Producte>();
+
+        private int nombre_productes;
+
+        private int nombre_productes_actual;
+
+        public Botiga (int n)
+        {
+
+            nombre_productes = n;
+
+
+
+        }
+
+        public bool AfegirProducte(Producte Producte)
+        {
+            bool afegit = false;
+            bool trobat = false;
+            int posBuscar = 0;
+
+
+            while ((prestatge[posBuscar] != null) || (trobat == false) && (nombre_productes_actual<nombre_productes))
+            {
+                if (prestatge[posBuscar] == null)
+                {
+                    trobat = true;
+                }
+
+                else
+                    posBuscar++;
+
+
+            }
+
+            if (trobat)
+            {
+                prestatge[posBuscar] = Producte;
+                afegit = true;
+                nombre_productes_actual++;
+                
+            }
+
+
+            return afegit;
+
+
+
+
+
+        }
+
+        public Producte BuscarProducte(Producte p)
+        {
+            Producte producte = null;
+            producte = prestatge.Find(a => a.nom.Equals(p.nom));
+            return (producte);
+        }
+
+
+    }
+}
---
+++ Botiga LListes/Producte.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botiga_LListes
+{
+    internal class Producte
+    {
+
+
+        public string nom { get; set; }
+        public double preu_sense_iva { get; set; }
+        public double iva { get; set; }
+
+
+
+        public Producte()
+        {
+
+
+        }
+        public Producte(string producte, double preu, double iva)
+        {
+            nom = producte;
+            preu_sense_iva = preu;
+            this.iva = iva;
+        }
+
+        public double Preu()
+        {
+            double preuTotal;
+
+            preuTotal = preu_sense_iva + (preu_sense_iva * iva / 100);
+
+
+            return preuTotal;
+
+
+        }
+
+        public string toString()
+        {
+            string text = $"Producte: {nom}, preu sense IVA: {preu_sense_iva}, preu amb IVA: {Preu()}, IVA aplicat: {iva}";
+
+
+            return text;
+        }
+
+
+
+
+    }
+}
